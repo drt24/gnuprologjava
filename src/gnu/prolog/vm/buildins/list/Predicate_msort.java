ab0ed2cdb42a15bdf7b9e560cd0b96c41b99f69a
elmuerte <elmuerte@c6d13eaa-423a-49d0-8e1d-62509e1234ed>
2009-06-23 14:48:21 +0000

    some sorting predicates
    git-svn-id: file:///home/daniel/dev/gnuprolog/gnuprolog-mod.plaster.svn@66 c6d13eaa-423a-49d0-8e1d-62509e1234ed

---
+++ src/gnu/prolog/vm/buildins/list/Predicate_msort.java
@@ -0,0 +1,86 @@
+/* GNU Prolog for Java
+ * Copyright (C) 1997-1999  Constantine Plotnikov
+ * Copyright (C) 2009       Michiel Hendriks
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA. The text ol license can be also found 
+ * at http://www.gnu.org/copyleft/lgpl.html
+ */
+package gnu.prolog.vm.buildins.list;
+
+import gnu.prolog.term.CompoundTerm;
+import gnu.prolog.term.Term;
+import gnu.prolog.term.TermComparator;
+import gnu.prolog.vm.Environment;
+import gnu.prolog.vm.Interpreter;
+import gnu.prolog.vm.PrologCode;
+import gnu.prolog.vm.PrologException;
+import gnu.prolog.vm.TermConstants;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 
+ * @author Michiel Hendriks
+ */
+public class Predicate_msort implements PrologCode
+{
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gnu.prolog.vm.PrologCode#execute(gnu.prolog.vm.Interpreter, boolean,
+	 * gnu.prolog.term.Term[])
+	 */
+	public int execute(Interpreter interpreter, boolean backtrackMode, Term[] args) throws PrologException
+	{
+		if (!CompoundTerm.isListPair(args[0]))
+		{
+			PrologException.typeError(TermConstants.listAtom, args[0]);
+		}
+		List<Term> list = new ArrayList<Term>();
+		CompoundTerm.toCollection(args[0], list);
+		Collections.sort(list, getComparator());
+		Term result = CompoundTerm.getList(list);
+		return interpreter.unify(args[1], result);
+	}
+
+	/**
+	 * @return
+	 */
+	protected Comparator<? super Term> getComparator()
+	{
+		return new TermComparator();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gnu.prolog.vm.PrologCode#install(gnu.prolog.vm.Environment)
+	 */
+	public void install(Environment env)
+	{}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gnu.prolog.vm.PrologCode#uninstall(gnu.prolog.vm.Environment)
+	 */
+	public void uninstall(Environment env)
+	{}
+
+}
