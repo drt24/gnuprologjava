f5692da78b9ee9c024a772802dd2892f5ce1f5c8
Daniel Thomas <drt24@cam.ac.uk>
2010-06-05 12:18:23 +0100

    Add top level documentation for the parser package and to TermParser.jj
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- src/gnu/prolog/io/parser/package-info.java
+++ src/gnu/prolog/io/parser/package-info.java
@@ -1,5 +1,14 @@
 /**
+ * This package contains the TermParser.jj file which is used by javacc to
+ * generate the parser found in {@link gnu.prolog.io.parser.gen}.
  * 
+ * There is also a TermParser.g file which is an in progress conversion of
+ * JavaCC grammar to antlr.
+ * 
+ * This package also contains classes such as {@link NameToken} and
+ * {@link ReaderCharStream} and {@link TermParserUtils} which contain some of
+ * the code needed to integrate the generated parser with the rest of the
+ * codebase.
  */
 package gnu.prolog.io.parser;
 
