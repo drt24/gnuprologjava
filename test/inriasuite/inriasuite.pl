f2aa8996c692c4090b5609842b7ba64b0387f823
Daniel Thomas <drt24@srcf.ucam.org>
2010-06-29 16:33:58 +0100

    Add some selftesting to the inria suite
    The inria suite lacks tests that check that it works and so I have added
    some.
    I also fixed some formatting and added a note of its reliance on
    append/3 and started extra/ to store tests on these extra predicates and
    inria to store tests on itself.
    
    Signed-off-by: Daniel Thomas <drt24@srcf.ucam.org>

--- test/inriasuite/inriasuite.pl
+++ test/inriasuite/inriasuite.pl
@@ -34,9 +34,7 @@
 %  tests.
 %
 
-
 :- op( 20, xfx, <-- ).
-
 
 
 %%%%%%%%%%%%%%%%%%%%
@@ -45,8 +43,6 @@
 % 
 %  score(File, total(Tests), wrong(PossibleErrors)
 %
-
-
 
 :- dynamic(score/3).
 
@@ -66,7 +62,6 @@
 %
 %   Driver.
 
-
 run_all_tests :-
 	findall(F, file(F), Files),
         test_all(Files),
@@ -77,6 +72,7 @@ test_all([]).
 test_all([F|Fs]) :-
 	run_tests(F),
         test_all(Fs).
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -100,8 +96,6 @@ write_results :-
         ).
 
 
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
 %
 %    result(+Goal, -Result)
@@ -114,20 +108,19 @@ result(G, Res) :-
       special_ans_forms(Subs,Res).
 
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   certain substitutions appear in
 %   a simplified form.
 %
 
-
 special_ans_forms([success], success) :- !.
 special_ans_forms([failure], failure) :- !.
 special_ans_forms([Error], Error) :-
 	Error =..[E |_],error_type(E), !.
 special_ans_forms(X,X).
-	
+
+
 %%%%%%%%%%%%%%%%
 %
 %   error_type(+E).
@@ -146,30 +139,24 @@ error_type(system_error).
 error_type(unexpected_ball).          % for uncaught errors.
 
 
-
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   Extract  the variables from a term
 %
-%   vars_int_term(+Term, -Vars)
+%   vars_in_term(+Term, -Vars)
 %
 
 vars_in_term(T,V) :-
 	vars_in_term(T, [], V).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
+%%%%
 %   vars_in_term(+Term, +AlreadyCollected, -Variables).
 %
 
-%  atoms (includes []).
+vars_in_term(Term,VarsIn, VarsIn) :-%VarsIn=VarsOut
+      atomic(Term), !.
 
-vars_in_term(Term,VarsIn, VarsOut) :-
-      atomic(Term), !, VarsOut= VarsIn.
 % Term  is a variable
-
 vars_in_term(Term, VarsIn, VarsOut) :-
 	var(Term) ,!, 
         (already_appears(Term, VarsIn)
@@ -180,18 +167,17 @@ vars_in_term(Term, VarsIn, VarsOut) :-
         ). 
 
 % Term is a list.
-
 vars_in_term([A|B], VarsIn, Vars) :-
         !, 
         vars_in_term(A, VarsIn, V1),
         vars_in_term(B, V1, Vars).
 
 % Term is a functor.
-
 vars_in_term(T,VarsIn, VarList) :-
        T =.. [_F,A|Args],
        vars_in_term(A, VarsIn, Inter),
        vars_in_term(Args, Inter, VarList).
+
 
 %%%%%%%
 %
@@ -199,17 +185,14 @@ vars_in_term(T,VarsIn, VarList) :-
 %
 %  The variable Var is in the list VarList
 %
- 
 
 already_appears(Var, [V1 | _Vlist] ) :-
 	Var == V1.
 already_appears(Var, [_V1 | Vlist] ) :-
         already_appears(Var, Vlist).
- 
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
 %
 %   call_goal_get_subs(+Goal, -Sub)
 %
@@ -217,15 +200,12 @@ already_appears(Var, [_V1 | Vlist] ) :-
 %   associated to success.
 %
 
-
 call_goal_get_subs(G, Sub) :-
         copy_term(G,GT),
         vars_in_term(G,Vars),
         vars_in_term(GT, GVars),
         call(GT),
         make_subs_list1(Vars, GVars, Sub).
-
-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -252,11 +232,9 @@ make_subs_list1(Vs,GVs,Sub) :-
       compress_sub_list(Vs, S, Sub).
 
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   make_subs_list(+Vars, +Result, -Subs).
-
 
 make_subs_list([],[], []).
 
@@ -270,6 +248,7 @@ make_subs_list([V | Rest], [Ans |ARest], Subs) :-
 
 make_subs_list([V | Rest], [Ans |ARest], [ V <-- Ans | SubsRest]) :-
          make_subs_list(Rest, ARest, SubsRest).
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -287,7 +266,7 @@ list_make_subs_list_aux(_Vars, [], []).
 list_make_subs_list_aux(Vars, [GV1 |GVRest], [Sub1 |SubRest]) :- 
          make_subs_list1(Vars, GV1, Sub1),
          list_make_subs_list_aux(Vars, GVRest, SubRest).
-	
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -303,7 +282,6 @@ call_with_result(_G, failure).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-%
 %  protected_call_results(G,R)
 %
 
@@ -312,8 +290,6 @@ protected_call_results(G,R) :-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%
 %
 %   get_all_subs(G, AllSubs)
 %
@@ -327,7 +303,6 @@ get_all_subs(G, AllSubs) :-
         vars_in_term(G, GVars),
         findall(GTAns, protect_call_result(GT, GTAns), GTAnsList),
         list_make_subs_list(GVars, GTAnsList, AllSubs).
-
 
 
 %%%%%%%%%%%%%%
@@ -344,8 +319,8 @@ call_result(G,R) :-
         
         R = GVars.
 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%
-%
 %
 %  protect_call_result(G,R)
 %
@@ -365,15 +340,12 @@ extract_error(error(R, _), R) :- !.
 extract_error(B, unexpected_ball(B)).
 
 
-
-
 %%%%%%%%%%%%%%%%%%%%%
 %
 %  compress_sub_list(+Vars, +LIn, -LOut)
 %
 %   to replace pairs [X <--A, Y <-- A] by [Y <-- X]
 %   when A is not one of the original variables.
-
 
 compress_sub_list(_, [], success).
 compress_sub_list(Vars, [X <-- A], [X <-- A]) :- X \== A, in_vars(A, Vars).
@@ -386,17 +358,18 @@ compress_sub_list(Vars,LIn, LOut) :-
       compress_sub_list(Vars, L1, LOut).
 
 compress_sub_list(_,L,L).
-      
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   in_vars(Var, VarList)
+%   is Var in VarList
 %
 
 in_vars(V, [V1 |_Vs]) :-
        V == V1, !.
 in_vars(V, [_V1 |Vs]) :-
       in_vars(V, Vs).	
-
 
 
 %%%%%%%%%%%%%%%%%%%
@@ -411,8 +384,8 @@ sub(X <-- A, [H|T], [H1|T1]) :-
 	sub1(X <-- A, H,H1),
         sub( X <-- A, T,T1).
 
+
 %%%%%%%%%%%%%%%%%%%%%
-%
 %
 %   sub1(X <-- A, Y <-- Old, Y <-- New)
 %
@@ -445,25 +418,18 @@ list_exp_sub(S, [E|ER], [EN|ERN]) :-
         list_exp_sub(S, ER, ERN).
 
 
-
 %%%%%%%%%%%%%%%%%%%%%%
-%
 %
 %   split_list(?Element,-Before, -After, +List)
 %
 % split a list List  at a given Element.
 %
 
-
 split_list(Element, Before, After, List) :-
 	append(Before, [Element | After], List).
-       
-	
-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%
-%
 %
 %   compare_subst_lists(+First,
 %                       +Second,
@@ -474,26 +440,32 @@ split_list(Element, Before, After, List) :-
 %
 
 % special cases
+% First and Second are not lists and First = Second
 compare_subst_lists(F,S, [],[]) :-
 	\+ (F = [_|_]),
         \+ (S = [_|_]), 
         F = S, !.
+% First and Second are not lists and because above did not match First \= Second
 compare_subst_lists(F,S, F,S) :-
       	\+ (F = [_|_]),
         \+ (S = [_|_]), !.
+% First is not a list remove it from the list (by above) Second giving SNF
+% If First is a member of Second then FNS=[] else FNS = First
 compare_subst_lists(F,S, FNS, SNF) :-
         \+(F = [_|_]), !,
        del_item(F, S, SNF),
       (member(F,S) -> FNS =[]; FNS = F).
+% Second is not a list and by above First is a list. Same as above.
 compare_subst_lists(F,S, FNS,SNF) :-
      \+( S = [_|_]), !,
       del_item(S, F, FNS),
       (member(S,F) -> SNF =[]; SNF = S).
-
+/* all these cases are contained in the last case
+% Both 1 element lists which are the same
 compare_subst_lists(F, S, [], []) :-
      F= [F1], S = [S1],
      same_subst(F1, S1), !.
- 
+% Both 1 element list which must be different
 compare_subst_lists(F, S, F, S) :-
      length(F,1),
      length(S,1), !. 
@@ -506,20 +478,28 @@ compare_subst_lists(F,S, FNS,SNF) :-
      length(S,1),
       del_item(S, F, FNS),
       (member(S,F) -> SNF =[]; SNF = S).
-
+*/
 
 compare_subst_lists(F,S, FNS, SNF) :-
 	list_del_item(F,S, SNF),
         list_del_item(S,F, FNS).
 
 
-%list_del_item(L1, L2, L2LessL1)
+%%%%%%
+%
+%  list_del_item(?L1, ?L2, ?L2LessL1)#
+%  L2LessL1 contains all the elements in L2 less all the elements in L1
 
 list_del_item([], L,L).
 list_del_item([It|R], L1, Left) :-
 	del_item(It, L1, LInter),
         list_del_item(R, LInter, Left).
-           
+
+
+%%%%%%%
+%
+%  del_item(+Item, +Rest, -Remainder)
+%  Remove Item from the list Rest leaving Remainder
 del_item(_Item, [],[]).
 del_item(Item, [It |R], R) :-
       same_subst(Item, It), ! .
@@ -527,7 +507,9 @@ del_item(Item, [It |R], R) :-
 del_item(Item, [It|Rest], [It |R]) :-
 	del_item(Item, Rest, R).
 
-%%
+
+%%%%%%
+%
 %  same_subst(Sub1, Sub2)
 %
 %  Sub1 and Sub2 represent the same subst.
@@ -537,6 +519,7 @@ same_subst([],[]).
 same_subst([S1|SRest], Subs) :-
         delmemb(S1, Subs, Subs1),
         same_subst(SRest, Subs1).
+
 
 %%%%%%%%%%
 %
@@ -548,7 +531,7 @@ same_subst([S1|SRest], Subs) :-
 delmemb(_E, [], []).
 delmemb(E <-- E1 , [F <-- F1| R], R) :-
          E == F, 
-	copy_term(E <-- E1 ,F <-- F1).  % only when LHS's are eq.
+	copy_term(E <-- E1 ,F <-- F1),!.  % only when LHS's are eq.
 delmemb(E, [F|R], [F|R1]) :-
       delmemb(E,R,R1).
 
@@ -568,19 +551,19 @@ read_test(Extra, Missing) :-
         compare_subst_lists(R, Expected, Extra, Missing),
         write('Extra Solutions found: '), write(Extra), nl,
         write('Solutions Missing: '), write(Missing).
-        
-        
+
+
 %%%%%%%%%
 %
 %   read tests from a file
 %
-
 
 run_tests(File) :-
         asserta(score(File, total(0), wrong(0))),
 	open(File, read, S),
         loop_through(File,S),
         close(S).
+
 
 %%%%%%%%%%%%%%%%%%%%
 %
@@ -590,7 +573,7 @@ run_tests(File) :-
 %    the catch is for syntax errors 
 %    (which will be errors in the processor).
 %
- 
+
 loop_through(F, S) :-
 	catch(read(S,X), B, X = B),
         (
@@ -601,6 +584,7 @@ loop_through(F, S) :-
         test(F,X),
         loop_through(F,S)
        ).
+
 
 %%%%%%%%%%%%%%%%%%%
 %
@@ -625,7 +609,9 @@ test(F, [G, ProgFile, Expected]) :-
         result(G,R),
         compare_subst_lists(R, Expected, Extra, Missing),
         write_if_wrong(F, G, Expected, Extra, Missing),
-        update_score(F, Missing, Extra). 
+        update_score(F, Missing, Extra).
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   write_if_wrong(+File, +Goal, +Expected, +Extra, +Missing)
@@ -646,7 +632,8 @@ write_if_wrong(F, G,Expected, Extra, Missing) :-
         write('Expected: '), write(Expected), nl,
         write('Extra Solutions found: '), write(Extra), nl,
         write('Solutions Missing: '), write(Missing),nl,nl.
- 
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %  update_score(+File, +Missing, +Extra)
@@ -654,7 +641,6 @@ write_if_wrong(F, G,Expected, Extra, Missing) :-
 %  add 1 to total in all cases.
 %  If Missing or Extra are non empty add one to wrong.
 %
-
 
 update_score(F,[],[]) :- !,
 	retract(score(F,total(T), wrong(W))),
@@ -666,11 +652,6 @@ update_score(F,_,_) :-
         asserta(score(F,total(T1), wrong(W1))). 
 
 
-
-
-
-
-
 %%%%%%%%%%%%%
 %
 %  inerror(?F)
@@ -679,12 +660,9 @@ update_score(F,_,_) :-
 % unexpected answer.
 %
 
-
-
 inerror(F) :-
 	score(F, total(_X), wrong(Y)),
         Y =\= 0.
-
 
 
 %%%%%%%%%
@@ -753,13 +731,20 @@ file('term_lt=').
 file(true).
 file(unify).
 
+% extras
+file('extra/append').
+%file('extra/').
 
-       
+% self test
+file('inria/already_appears').
+file('inria/vars_in_term').
+file('inria/delmemb').
+%file('inria/').
+
 %%%%%%%%%%%
 %
 %   display_list(+List)
 %
-
 
 display_list([]) :- nl.
 display_list([H|T]) :-
@@ -775,9 +760,9 @@ display_list([H|T]) :-
 % in order to fix this we restore them to their default values.
 % This is why fail is the first test.
 
-
 reset_flags :-
 	set_prolog_flag(unknown, error).
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -795,7 +780,6 @@ exists(P/I) :-
 exists(P/I) :-
       write('Predicate: '), write(P/I), write(' not implemented'), nl,
       reset_flags.
-
 
 
 %%%%%%%%%%%%%
