f3e244c6a58bf635b39e4ed70d8c6cfaab46482f
Daniel Thomas <drt24@srcf.ucam.org>
2010-07-21 22:07:42 +0100

    Add Term which varies value at runtime
    As part of adding support for double_quotes add support for terms which
    change their values at runtime with ChangeableTerm and then extend that
    with DoubleQuotesTerm to make a term for use with solving the
    double_quotes problem.
    
    Signed-off-by: Daniel Thomas <drt24@srcf.ucam.org>

---
+++ src/gnu/prolog/term/ChangeableTerm.java
@@ -0,0 +1,53 @@
+/* GNU Prolog for Java
+ * Copyright (C) 2010  Daniel Thomas
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA. The text of license can be also found 
+ * at http://www.gnu.org/copyleft/lgpl.html
+ */
+package gnu.prolog.term;
+
+import gnu.prolog.vm.Environment;
+
+/**
+ * Terms for which the value varies depending on prolog flags extend this class.
+ * They pick which to use for value on creation and at {@link #dereference()}.
+ * 
+ * @author Daniel Thomas
+ */
+public abstract class ChangeableTerm extends VariableTerm
+{
+	private static final long serialVersionUID = 3298672894800988968L;
+
+	/**
+	 * The Environment we will use to get the flags from.
+	 */
+	protected Environment environment;
+
+	protected Term getPrologFlag(AtomTerm term)
+	{
+		return environment.getPrologFlag(term);
+	}
+
+	protected ChangeableTerm(Environment env)
+	{
+		environment = env;
+	}
+
+	/**
+	 * The value may also be altered by this method in child classes.
+	 */
+	@Override
+	public abstract Term dereference();
+}
---
+++ src/gnu/prolog/term/DoubleQuotesTerm.java
@@ -0,0 +1,77 @@
+/* GNU Prolog for Java
+ * Copyright (C) 2010  Daniel Thomas
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA. The text of license can be also found 
+ * at http://www.gnu.org/copyleft/lgpl.html
+ */
+package gnu.prolog.term;
+
+import gnu.prolog.vm.Environment;
+import gnu.prolog.vm.Environment.DoubleQuotesValue;
+
+/**
+ * Term for storing a double quoted string as what this resolves to varies
+ * depending on the value of the double_quotes flag.
+ * 
+ * @author Daniel Thomas
+ */
+public class DoubleQuotesTerm extends ChangeableTerm
+{
+	private static final long serialVersionUID = 4760147509009342072L;
+
+	private CompoundTerm codesValue;
+	private CompoundTerm charsValue;
+	private AtomTerm atomValue;
+
+	/**
+	 * Construct a DoubleQuotesTerm
+	 * 
+	 * @param environment
+	 *          the environment to be used to get the prolog flags from
+	 * @param codes
+	 *          the char codes representation e.g. [232,134]
+	 * @param chars
+	 *          the chars representation e.g. [a,b]
+	 * @param atom
+	 *          the atom representation e.g. ab
+	 */
+	public DoubleQuotesTerm(Environment environment, CompoundTerm codes, CompoundTerm chars, AtomTerm atom)
+	{
+		super(environment);
+
+		codesValue = codes;
+		charsValue = chars;
+		atomValue = atom;
+
+		dereference();// sets value as a side effect.
+	}
+
+	@Override
+	public Term dereference()
+	{
+		DoubleQuotesValue status = DoubleQuotesValue.fromAtom((AtomTerm) getPrologFlag(Environment.doubleQuotesAtom));
+		switch (status)
+		{
+			case DQ_ATOM:
+				value = atomValue;
+			case DQ_CHARS:
+				value = charsValue;
+			case DQ_CODES:
+				value = codesValue;
+		}
+		return value;
+	}
+
+}
