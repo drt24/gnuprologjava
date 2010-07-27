1f6ad63f1cbfe5a68bfd641f3d18ab4523c3b3e0
Daniel Thomas <drt24@srcf.ucam.org>
2010-07-27 14:16:05 +0100

    Remove outdated test suite .cmd files
    We have ant tasks now which do a better job (and work on GNU/Linux).
    
    Signed-off-by: Daniel Thomas <drt24@srcf.ucam.org>

--- test/suite.cmd
+++
@@ -1,4 +0,0 @@
-cd vanilla
-@attrib -R *.*
-java -jar ..\..\build\gnuprolog.jar -once gnuprolog.pl validate >..\suite.out 2>..\suite.err
-cd ..
--- test/vanilla.cmd
+++
@@ -1,4 +0,0 @@
-cd vanilla_stock
-@attrib -R *.*
-java -jar ..\..\build\gnuprolog.jar -once gnuprologjava.pl validate >..\test.out 2>..\test.err
-cd ..
