29973b694e0b3d980909de0c0739dde5f848e8ea
Rishabh Garg <rishabhgarg.as@gmail.com>
2013-09-17 04:12:19 +0530

    Add note about modifying the parser.
    Contains information as to how to go about generating the /gen files using the .jj file.

--- src/gnu/prolog/io/parser/package-info.java
+++ src/gnu/prolog/io/parser/package-info.java
@@ -1,23 +1,50 @@
 /**
- * This package contains the TermParser.jj file which is used by javacc to
+ * This package contains the TermParser.jj file which is used by javacc 5 to
  * generate the parser found in {@link gnu.prolog.io.parser.gen}.
  * 
- * Unfortunately modern versions of javacc cannot be used to regenerate the
- * parser as the parser becomes non-terminating. When you wish to modify
- * something in .gen then you need to also modify the .jj file - if you can't
- * reflect your changes there then don't make those changes.
- * 
- * There is also a TermParser.g file which is an in progress conversion of
- * JavaCC grammar to antlr.
+ * You only need to make changes in the TermParser.jj file which will reflect
+ * the changes automatically in the .gen files and you are done. 
+ *
+ * However, on using javacc TermParser.jj, you need to make the following changes:
+ * 1. The first line of the code of JavaCharStream.java needs to implement CharStream:
+ *      --package gnu.prolog.io.parser.gen;
+ *	++package gnu.prolog.io.parser;
+ *   	++import gnu.prolog.io.parser.gen.CharStream;
+ *   	--public class JavaCharStream
+ *	++public class JavaCharStream implements CharStream 
+ *      ...
+ *
+ * 2. The method jjFillToken in TermParserToken manager should include the following lines:
+ *   	protected Token jjFillToken()
+ *   	...
+ *   	t = Token.newToken(jjmatchedKind, curTokenImage);
+ *   	++t.kind = jjmatchedKind;
+ *   	++t.image = curTokenImage;
+ *   	...
+ *
+ * 3. The file Token.java needs the addition of the following two lines of code:
+ *	public static Token newToken(int ofKind, String image)
+ * 	{
+ *   		switch(ofKind)
+ *   		{
+ *   			++case TermParserConstants.NAME_TOKEN:
+ *				++return new gnu.prolog.io.parser.NameToken();
+ *     			default : return new Token(ofKind, image);
+ *   		}
+ * 	}
+ *
+ * Finding a way so that the .jj file can be edited to get the above changes on its 
+ * own could be an improvement and ideas to do the same are welcome by users.
+ *
+ * There is also a parser branch on github which is the rewriting of parser
+ * in ANTLR 4 but ANTLR 4 does not support left-recursion with parameters
  * 
  * This package also contains classes such as
  * {@link gnu.prolog.io.parser.NameToken NameToken},
- * {@link gnu.prolog.io.parser.ReaderCharStream ReaderCharStream} and
+ * {@link gnu.prolog.io.parser.JavaCharStream JavaCharStream} and
  * {@link gnu.prolog.io.parser.TermParserUtils TermParserUtils} which contain
  * some of the code needed to integrate the generated parser with the rest of
  * the codebase.
  * 
- * TODO: parser does not properly handle unicode even in comments @see ReaderCharStream
  */
 package gnu.prolog.io.parser;
-
